cc7077df407a0e9e05668c2839418f8ed99a5fb0
Dharani0703 <gudurudharani7@gmail.com>
2024-05-31 17:56:30 +0530

    first commit

---
+++ src/day1/hello.java
@@ -0,0 +1,26 @@
+package day1;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		Scanner sc - new Scanner(System.in);
+		a-sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("Add:"+c);
+		c=a-b;
+		System.out.println("sub:"+c);
+		c=a*b;
+		System.out.println("Mul:"+c);
+		c=a/b;
+		System.out.println("Div:"+c);
+		c=a%b;
+		System.out.println("Mod:"+c);
+		
+	}
+
+}
---
+++ src/operators/ArithassignEx.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class ArithassignEx {
+
+	public static void main(String[] args) {
+		int a=10,b=3;
+		a+=b;//a=a+b;
+		System.out.println(a);
+		a-=b;//a=a-b
+		System.out.println(a);
+		a*=b;//a=a*b
+		System.out.println(a);
+
+	}
+
+}
